b29c10b2bd9d6b2755144e3fda45f0b0128bd8e1
tdi-odoo <tdi@odoo.com>
2018-06-25 14:15:55 +0800

    [FIX] re-add standard behavior as second poissibility if no shipping method

--- purchase_discount/models/sale.py
+++ purchase_discount/models/sale.py
@@ -112,6 +112,21 @@ class SaleOrderLine(models.Model):
             M = pricelist_item[0].price_min_margin
 
             self.price_unit = (DPC + LC) * (1 + M / 100.0)
+        else:
+            if not self.product_uom or not self.product_id:
+                self.price_unit = 0.0
+                return
+            if self.order_id.pricelist_id and self.order_id.partner_id:
+                product = self.product_id.with_context(
+                    lang=self.order_id.partner_id.lang,
+                    partner=self.order_id.partner_id.id,
+                    quantity=self.product_uom_qty,
+                    date=self.order_id.date_order,
+                    pricelist=self.order_id.pricelist_id.id,
+                    uom=self.product_uom.id,
+                    fiscal_position=self.env.context.get('fiscal_position')
+                )
+                self.price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
 
     @api.onchange('product_uom', 'product_uom_qty')
     def product_uom_change(self):
