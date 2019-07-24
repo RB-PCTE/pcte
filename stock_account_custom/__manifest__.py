16ee681e1abc1961e760b728fcd10f6e300e6b32
Lap Dang <xuanlapd@gmail.com>
2019-07-24 16:29:54 +0700

    [MOD] apply discount for price unit in stock move

---
+++ stock_account_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Stock Account Custom - PCTE',
+    'version': '1.1',
+    'category': 'Account',
+    'description': """
+Custom stock account for PCTE
+
+    """,
+    'author': "Havi Technology",
+    'website': "havi.com.au",
+    'depends': ['stock_account','purchase_discount'],
+    'data': [
+    ],
+}
---
+++ stock_account_custom/models/purchase_order.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models, fields, exceptions, api, _
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.multi
+    def _get_stock_move_price_unit(self):
+        self.ensure_one()
+        line = self[0]
+        order = line.order_id
+        if line.discount and line.discount !=0:
+            price_unit = line.price_unit*(1-(line.discount/100))
+        else:
+            price_unit = line.price_unit
+        if line.taxes_id:
+            price_unit = line.taxes_id.with_context(round=False).compute_all(
+                price_unit, currency=line.order_id.currency_id, quantity=1.0, product=line.product_id, partner=line.order_id.partner_id
+            )['total_excluded']
+        if line.product_uom.id != line.product_id.uom_id.id:
+            price_unit *= line.product_uom.factor / line.product_id.uom_id.factor
+        if order.currency_id != order.company_id.currency_id:
+            price_unit = order.currency_id._convert(
+                price_unit, order.company_id.currency_id, self.company_id, self.date_order or fields.Date.today(), round=False)
+        return price_unit
