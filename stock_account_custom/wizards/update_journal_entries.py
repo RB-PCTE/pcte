1a4d845a99d7597973c3069231ffea9ef10fd1a1
Lap Dang <xuanlapd@gmail.com>
2019-07-30 10:58:31 +0700

    [MOD] fix journal entries

--- stock_account_custom/wizards/update_journal_entries.py
+++ stock_account_custom/wizards/update_journal_entries.py
@@ -33,7 +33,7 @@ class WizardUpdateJournalEntries(models.TransientModel):
                     _logger.info("[MOVE NAME] {}".format(move.name))
                     if move.stock_move_id.purchase_line_id and move.stock_move_id.purchase_line_id.discount and move.stock_move_id.purchase_line_id.discount !=0 and move.price_unit:
                         _logger.info("[INCOMING] Price before change {}".format(move.stock_move_id.price_unit))
-                        new_price = move.price_unit*(1-(move.stock_move_id.purchase_line_id.discount/100))
+                        new_price = move.stock_move_id.price_unit*(1-(move.stock_move_id.purchase_line_id.discount/100))
                         move.stock_move_id.write({'price_unit': new_price})
                         for move_line in move.line_ids:
                             if move_line.credit != 0:
@@ -44,7 +44,7 @@ class WizardUpdateJournalEntries(models.TransientModel):
                     else:
                         if move.stock_move_id.product_id.supplier_discount and move.stock_move_id.picking_type_id.code == 'outgoing':
                             _logger.info("[OUTGOING] Price before change {}".format(move.stock_move_id.price_unit))
-                            new_price = move.price_unit * (1 - (move.stock_move_id.product_id.supplier_discount / 100))
+                            new_price = move.stock_move_id.price_unit * (1 - (move.stock_move_id.product_id.supplier_discount / 100))
                             move.stock_move_id.write({'price_unit': new_price})
                             for move_line in move.line_ids:
                                 if move_line.credit != 0:
