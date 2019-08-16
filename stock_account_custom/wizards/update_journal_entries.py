eca63add24189911aa5f4d0908259aaab12e47eb
Lap Dang <xuanlapd@gmail.com>
2019-08-16 15:18:47 +0700

    [IMP] new build

--- stock_account_custom/wizards/update_journal_entries.py
+++ stock_account_custom/wizards/update_journal_entries.py
@@ -79,7 +79,7 @@ class WizardUpdateJournalEntries(models.TransientModel):
                                 if (move_line.account_id.id == move_line.product_id.product_tmpl_id.categ_id.property_account_expense_categ_id.id or move_line.account_id.code in expense_account_code) and move_line.debit !=0.0:
                                     self._cr.execute("""UPDATE  account_move_line set debit=%s,balance=%s WHERE id=%s""",(price * move_line.quantity,price * move_line.quantity, move_line.id))
                                 if move_line.account_id.id == move_line.product_id.product_tmpl_id.categ_id.property_stock_account_output_categ_id.id and move_line.credit !=0.0:
-                                    self._cr.execute("""UPDATE  account_move_line set credit=%s, balance=%s WHERE id=%s""",(price * move_line.quantity,price * move_line.quantity, move_line.id))
+                                    self._cr.execute("""UPDATE  account_move_line set credit=%s, balance=%s WHERE  id=%s""",(price * move_line.quantity,price * move_line.quantity, move_line.id))
                         move._amount_compute()
                         move._post_validate()
                         move.post()
