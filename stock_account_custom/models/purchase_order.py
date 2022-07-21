d1b27c5f0d03d57eac7d83cb37abf029265f5ee3
Vishnu Vanneri <vvanneri@ioppolo.com.au>
2022-07-21 09:57:30 +0530

    [MIG] stock_account_custom: code migrated to v15

--- stock_account_custom/models/purchase_order.py
+++ stock_account_custom/models/purchase_order.py
@@ -1,37 +1,31 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, fields, exceptions, api, _
-import logging
-_logger = logging.getLogger(__name__)
+from odoo import models
+from odoo.tools.float_utils import float_round
+
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    @api.multi
     def _get_stock_move_price_unit(self):
         use_supplier_price = self.env['ir.config_parameter'].sudo().get_param('stock_account_custom.use_supplier_price')
-        if use_supplier_price:
-            super(PurchaseOrderLine, self)._get_stock_move_price_unit()
+        if use_supplier_price and self.discount:
             self.ensure_one()
-            line = self[0]
-            order = line.order_id
-            _logger.info("price unit is {}".format(line.price_unit))
-            if line.discount and line.discount !=0:
-                price_unit = line.price_unit*(1-(line.discount/100))
-            else:
-                price_unit = line.price_unit
-            if line.taxes_id:
-                price_unit = line.taxes_id.with_context(round=False).compute_all(
-                    price_unit, currency=line.order_id.currency_id, quantity=1.0, product=line.product_id,
-                    partner=line.order_id.partner_id
-                )['total_excluded']
-            if line.product_uom.id != line.product_id.uom_id.id:
-                price_unit *= line.product_uom.factor / line.product_id.uom_id.factor
+            order = self.order_id
+            price_unit = self.price_unit * (1 - (self.discount / 100))
+            price_unit_prec = self.env['decimal.precision'].precision_get('Product Price')
+            if self.taxes_id:
+                qty = self.product_qty or 1
+                price_unit = self.taxes_id.with_context(round=False).compute_all(
+                    price_unit, currency=self.order_id.currency_id, quantity=qty, product=self.product_id,
+                    partner=self.order_id.partner_id
+                )['total_void']
+                price_unit = float_round(price_unit / qty, precision_digits=price_unit_prec)
+            if self.product_uom.id != self.product_id.uom_id.id:
+                price_unit *= self.product_uom.factor / self.product_id.uom_id.factor
             if order.currency_id != order.company_id.currency_id:
-                price_unit = order.currency_id.with_context(date=order.date_approve).compute(price_unit,
-                                                                                             order.company_id.currency_id,
-                                                                                             round=False)
-            _logger.info("final price is {}".format(price_unit))
+                price_unit = order.currency_id._convert(
+                    price_unit, order.company_id.currency_id, self.company_id, self.date_order or fields.Date.today(),
+                    round=False)
             return price_unit
-        else:
-            return super(PurchaseOrderLine, self)._get_stock_move_price_unit()
+        return super(PurchaseOrderLine, self)._get_stock_move_price_unit()
