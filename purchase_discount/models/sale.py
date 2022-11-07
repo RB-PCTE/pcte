90021cf3df59373dbfc38eb28389266f2f706bae
Vishnu Vanneri <vvanneri@ioppolo.com.au>
2022-11-07 12:07:31 +0530

    [IMP] purchase_discount: fixed sale price calculation

--- purchase_discount/models/sale.py
+++ purchase_discount/models/sale.py
@@ -32,53 +32,11 @@ class SaleOrderLine(models.Model):
                 raise ValidationError("Did not find shipping information for product %s" % product.name)
         return amount
 
-    @api.onchange('product_id')
-    def product_id_change(self):
-        if not self.product_id:
-            return {'domain': {'product_uom': []}}
+    def _update_taxes(self):
+        super(SaleOrderLine, self)._update_taxes()
+        self._update_selling_price()
 
-        vals = {}
-        domain = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
-        if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
-            vals['product_uom'] = self.product_id.uom_id
-            vals['product_uom_qty'] = 1.0
-
-        product = self.product_id.with_context(
-            lang=self.order_id.partner_id.lang,
-            partner=self.order_id.partner_id.id,
-            quantity=vals.get('product_uom_qty') or self.product_uom_qty,
-            date=self.order_id.date_order,
-            pricelist=self.order_id.pricelist_id.id,
-            uom=self.product_uom.id
-        )
-
-        result = {'domain': domain}
-
-        title = False
-        message = False
-        warning = {}
-        if product.sale_line_warn != 'no-message':
-            title = _("Warning for %s") % product.name
-            message = product.sale_line_warn_msg
-            warning['title'] = title
-            warning['message'] = message
-            result = {'warning': warning}
-            if product.sale_line_warn == 'block':
-                self.product_id = False
-                return result
-
-        name = product.name_get()[0][1]
-        if product.description_sale:
-            name += '\n' + product.description_sale
-        vals['name'] = name
-
-        self._compute_tax_id()
-        self.update(vals)
-
-        return result
-
-    @api.onchange('product_id', 'product_uom_qty', 'shipping_type')
-    def get_selling_price(self):
+    def _update_selling_price(self):
         if self.product_id and self.shipping_type:
             PI = self.product_id.seller_ids and self.product_id.seller_ids[0].price or self.product_id.standard_price
             currency = self.product_id.seller_ids and self.product_id.seller_ids[0].currency_id
@@ -110,25 +68,11 @@ class SaleOrderLine(models.Model):
             M = pricelist_item[0].margin_pcte
 
             self.price_unit = (DPC + LC) * (1 + M / 100.0)
-        else:
-            if not self.product_uom or not self.product_id:
-                self.price_unit = 0.0
-                return
-            if self.order_id.pricelist_id and self.order_id.partner_id:
-                product = self.product_id.with_context(
-                    lang=self.order_id.partner_id.lang,
-                    partner=self.order_id.partner_id.id,
-                    quantity=self.product_uom_qty,
-                    date=self.order_id.date_order,
-                    pricelist=self.order_id.pricelist_id.id,
-                    uom=self.product_uom.id,
-                    fiscal_position=self.env.context.get('fiscal_position')
-                )
-                self.price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
 
-    @api.onchange('product_uom', 'product_uom_qty')
+    @api.onchange('product_uom', 'product_uom_qty', 'shipping_type')
     def product_uom_change(self):
-        pass
+        super().product_uom_change()
+        self._update_selling_price()
 
 
 class PricelistItem(models.Model):
